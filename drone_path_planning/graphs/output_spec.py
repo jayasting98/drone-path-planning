f47841ced55ab9fd12a0c9ebf6737abdda8808f8
Jay Aljelo Ting <65202977+jayasting98@users.noreply.github.com>
2023-01-04 19:10:33 +0800

    Convert graph output spec to named tuple

--- drone_path_planning/graphs/output_spec.py
+++ drone_path_planning/graphs/output_spec.py
@@ -1,6 +1,6 @@
-import dataclasses
 from typing import Dict
 from typing import List
+from typing import NamedTuple
 
 from drone_path_planning.graphs.edge_set import EdgeSet
 from drone_path_planning.graphs.node_set import NodeSet
@@ -10,7 +10,6 @@ OutputHeadSpec = Dict[str, int]
 OutputComponentSetSpec = List[OutputHeadSpec]
 
 
-@dataclasses.dataclass
-class OutputGraphSpec:
+class OutputGraphSpec(NamedTuple):
     node_sets: Dict[str, OutputComponentSetSpec]
     edge_sets: Dict[str, OutputComponentSetSpec]
