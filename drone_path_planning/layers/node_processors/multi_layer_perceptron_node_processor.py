78301f068831e9bfd162984edd18d3b6eadf2d71
Jay Aljelo Ting <65202977+jayasting98@users.noreply.github.com>
2023-01-04 14:11:11 +0800

    Add multi layer perceptron node processor

---
+++ drone_path_planning/layers/node_processors/multi_layer_perceptron_node_processor.py
@@ -0,0 +1,32 @@
+import tensorflow as tf
+
+from drone_path_planning.layers.basic_layers import MultiLayerPerceptron
+from drone_path_planning.layers.node_processors.node_processor import NodeProcessor
+
+
+class MultiLayerPerceptronNodeProcessor(NodeProcessor):
+    def _create_node_layer(
+        latent_size: int,
+        num_hidden_layers: int,
+        *args,
+        activation=None,
+        use_bias: bool = True,
+        kernel_regularizer=None,
+        bias_regularizer=None,
+        activity_regularizer=None,
+        should_layer_normalize=False,
+        **kwargs,
+    ) -> tf.keras.layers.Layer:
+        return MultiLayerPerceptron(
+            latent_size,
+            latent_size,
+            num_hidden_layers,
+            *args,
+            activation=activation,
+            use_bias=use_bias,
+            kernel_regularizer=kernel_regularizer,
+            bias_regularizer=bias_regularizer,
+            activity_regularizer=activity_regularizer,
+            should_layer_normalize=should_layer_normalize,
+            **kwargs,
+        )
