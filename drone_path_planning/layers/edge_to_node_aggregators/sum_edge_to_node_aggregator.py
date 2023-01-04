f2d0508a560dec94cd6a4df21d56e6e4ff7ea960
Jay Aljelo Ting <65202977+jayasting98@users.noreply.github.com>
2023-01-04 12:36:42 +0800

    Add sum edge-to-node aggregator

---
+++ drone_path_planning/layers/edge_to_node_aggregators/sum_edge_to_node_aggregator.py
@@ -0,0 +1,16 @@
+import tensorflow as tf
+
+from drone_path_planning.layers.edge_to_node_aggregators.edge_to_node_aggregator import EdgeToNodeAggregator
+
+
+class SumEdgeToNodeAggregator(EdgeToNodeAggregator):
+    def call(self, inputs, *args, **kwargs):
+        edge_set, node_count = inputs
+        edge_features = edge_set.features
+        receiver_nodes = edge_set.receivers
+        aggregated_partial_messages = tf.math.unsorted_segment_sum(
+            edge_features,
+            receiver_nodes,
+            node_count,
+        )
+        return aggregated_partial_messages
