e1a760bc9abdefce63671bfa4587a168f8fc3e70
Jay Aljelo Ting <65202977+jayasting98@users.noreply.github.com>
2023-01-05 12:17:35 +0800

    Add common function to find drone direction

---
+++ drone_path_planning/environments/common.py
@@ -0,0 +1,10 @@
+import tensorflow as tf
+
+
+def find_direction(angular_displacement: tf.Tensor):
+  direction = tf.concat([
+      tf.math.cos(angular_displacement),
+      tf.math.sin(angular_displacement),
+      tf.zeros(tf.shape(angular_displacement)),
+  ], axis=-1)
+  return direction
