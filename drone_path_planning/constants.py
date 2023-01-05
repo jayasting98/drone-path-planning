746b4081551e809a07ebdd8ca33bbc2f84e58b66
Jay Aljelo Ting <65202977+jayasting98@users.noreply.github.com>
2023-01-05 10:59:14 +0800

    Add observables as constants

---
+++ drone_path_planning/constants.py
@@ -0,0 +1,4 @@
+SELF_ANGULAR_VELOCITY: str = 'self_angular_velocity'
+TARGET_ANGULAR_VELOCITY: str = 'target_angular_velocity'
+TARGET_RELATIVE_DISPLACMENT: str = 'target_relative_displacement'
+TARGET_RELATIVE_VELOCITY: str = 'target_relative_velocity'
