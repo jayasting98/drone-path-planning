f17095d3b3913363d6cfc5f11b2c6c291ea95c89
Jay Aljelo Ting <65202977+jayasting98@users.noreply.github.com>
2023-01-05 11:29:43 +0800

    Add single-chaser single-target graph Q-network

---
+++ drone_path_planning/models/single_chaser_single_target_graph_q_network.py
@@ -0,0 +1,114 @@
+from typing import Dict
+
+import tensorflow as tf
+
+from drone_path_planning.constants import SELF
+from drone_path_planning.constants import TARGET
+from drone_path_planning.constants import SELF_ANGULAR_VELOCITY
+from drone_path_planning.constants import SELF_TARGET
+from drone_path_planning.constants import TARGET_ANGULAR_VELOCITY
+from drone_path_planning.constants import TARGET_RELATIVE_DISPLACMENT
+from drone_path_planning.constants import TARGET_RELATIVE_VELOCITY
+from drone_path_planning.constants import TARGET_SELF
+from drone_path_planning.graphs import EdgeSet
+from drone_path_planning.graphs import Graph
+from drone_path_planning.graphs import NodeSet
+from drone_path_planning.graphs import OutputGraphSpec
+from drone_path_planning.layers.encode_process_decodes import MultiLayerPerceptronEncodeProcessDecode
+
+
+class SingleChaserSingleTargetGraphQNetwork(tf.keras.layers.Layer):
+    def __init__(
+        self,
+        output_specs: OutputGraphSpec,
+        latent_size: int,
+        num_hidden_layers: int,
+        num_message_passing_steps: int,
+        *args,
+        should_normalize=False,
+        **kwargs,
+    ):
+        super().__init__(*args, **kwargs)
+        self._learned_model = MultiLayerPerceptronEncodeProcessDecode(
+            output_specs,
+            latent_size,
+            num_hidden_layers,
+            num_message_passing_steps,
+            should_normalize=should_normalize,
+        )
+
+    def call(self, inputs: Dict[str, tf.Tensor]):
+        graph = self._build_graph(inputs)
+        raw_predictions: Graph = self._learned_model(graph)
+        predictions = self._postprocess(raw_predictions)
+        return predictions
+
+    def _build_graph(self, inputs: Dict[str, tf.Tensor]) -> Graph:
+        self_node_set: NodeSet = self._build_self_node_set(inputs[SELF_ANGULAR_VELOCITY])
+        target_node_set: NodeSet = self._build_target_node_set(inputs[TARGET_RELATIVE_VELOCITY], inputs[TARGET_ANGULAR_VELOCITY])
+        target_relative_displacement = inputs[TARGET_RELATIVE_DISPLACMENT]
+        num_targets = tf.shape(target_relative_displacement)[0]
+        targets = tf.range(num_targets)
+        selves = tf.zeros_like(targets)
+        self_target_edge_set: EdgeSet = self._build_self_target_edge_set(target_relative_displacement, selves, targets)
+        target_self_edge_set: EdgeSet = self._build_target_self_edge_set(target_relative_displacement, targets, selves)
+        node_sets = {
+            SELF: self_node_set,
+            TARGET: target_node_set,
+        }
+        edge_sets = {
+            SELF_TARGET: self_target_edge_set,
+            TARGET_SELF: target_self_edge_set,
+        }
+        graph = Graph(node_sets, edge_sets)
+        return graph
+
+    def _postprocess(self, raw_predictions: Graph) -> Dict[str, tf.Tensor]:
+        predictions = raw_predictions.node_sets[SELF].features
+        return predictions
+
+    def _build_self_node_set(self_angular_velocity: tf.Tensor) -> NodeSet:
+        self_node_features = tf.concat([
+            self_angular_velocity,
+            tf.norm(self_angular_velocity, axis=-1, keepdims=True),
+        ], axis=-1)
+        self_node_set = NodeSet(self_node_features)
+        return self_node_set
+
+    def _build_target_node_set(target_relative_velocity: tf.Tensor, target_angular_velocity: tf.Tensor) -> NodeSet:
+        target_node_features = tf.concat([
+            target_relative_velocity,
+            tf.norm(target_relative_velocity, axis=-1, keepdims=True),
+            target_angular_velocity,
+            tf.norm(target_angular_velocity, axis=-1, keepdims=True),
+        ], axis=-1)
+        target_node_set = NodeSet(target_node_features)
+        return target_node_set
+
+    def _build_self_target_edge_set(target_relative_displacement: tf.Tensor, selves: tf.Tensor, targets: tf.Tensor) -> EdgeSet:
+        self_target_edge_features = tf.concat([
+            target_relative_displacement,
+            tf.norm(target_relative_displacement, axis=-1, keepdims=True),
+        ], axis=-1)
+        self_target_edge_set = EdgeSet(
+            self_target_edge_features,
+            selves,
+            targets,
+            SELF,
+            TARGET,
+        )
+        return self_target_edge_set
+
+    def _build_target_self_edge_set(target_relative_displacement: tf.Tensor, targets: tf.Tensor, selves: tf.Tensor) -> EdgeSet:
+        target_self_edge_features = tf.concat([
+            -target_relative_displacement,
+            tf.norm(-target_relative_displacement, axis=-1, keepdims=True),
+        ], axis=-1)
+        target_self_edge_set = EdgeSet(
+            target_self_edge_features,
+            targets,
+            selves,
+            TARGET,
+            SELF,
+        )
+        return target_self_edge_set
