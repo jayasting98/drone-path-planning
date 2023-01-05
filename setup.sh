22cbc5be74bf21098b21a476b4ebe699b5137242
Jay Aljelo Ting <65202977+jayasting98@users.noreply.github.com>
2023-01-05 22:20:54 +0800

    Update setup.sh with preferences for CUDA or DirectML

--- setup.sh
+++ setup.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
-python3.8 -m venv venv
-source venv/bin/activate
-pip install -r requirements.txt
+case ${1:-'cuda'} in
+
+    'cuda')
+    python3.8 -m venv venvs/cuda
+    source venvs/cuda/bin/activate
+    pip install -r requirements/cuda.txt
+    ;;
+
+    'directml')
+    python3.8 -m venv venvs/directml
+    source venvs/directml/bin/activate
+    pip install -r requirements/directml.txt
+    ;;
+
+    *)
+    echo 'usage: bash setup.sh {cuda,directml}'
+    ;;
+
+esac
