e837f55a9a8107fa38b59a7515552ac2d31086e6
Jay Aljelo Ting <65202977+jayasting98@users.noreply.github.com>
2023-01-06 23:10:57 +0800

    Add trainer

---
+++ drone_path_planning/trainers/trainer.py
@@ -0,0 +1,29 @@
+import abc
+from typing import List
+
+import tensorflow as tf
+
+
+class Trainer:
+    @abc.abstractmethod
+    def initialize(self):
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def train(self):
+        raise NotImplementedError()
+
+    def _create_training_callbacks(self, num_epochs: int, num_steps_per_epoch: int) -> List[tf.keras.callbacks.Callback]:
+        callbacks = []
+        progbar_logger_callback = tf.keras.callbacks.ProgbarLogger(
+            count_mode='steps',
+            stateful_metrics=['epsilon', 'num_episodes'],
+        )
+        progbar_logger_callback_params = {
+            'verbose': 1,
+            'epochs': num_epochs,
+            'steps': num_steps_per_epoch,
+        }
+        progbar_logger_callback.set_params(progbar_logger_callback_params)
+        callbacks.append(progbar_logger_callback)
+        return callbacks
