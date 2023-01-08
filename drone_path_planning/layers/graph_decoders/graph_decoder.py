99ae3d493b440dd07156217c250bd2e489027065
Jay Aljelo Ting <65202977+jayasting98@users.noreply.github.com>
2023-01-08 17:27:18 +0800

    Make graph decoder serializable

--- drone_path_planning/layers/graph_decoders/graph_decoder.py
+++ drone_path_planning/layers/graph_decoders/graph_decoder.py
@@ -1,4 +1,5 @@
 import abc
+from typing import Any
 from typing import Dict
 from typing import Sequence
 from typing import TypeVar
@@ -20,6 +21,7 @@ SetDecoderMap = Dict[str, SetDecoder]
 T = TypeVar('T', bound=ComponentSet)
 
 
+@tf.keras.utils.register_keras_serializable('drone_path_planning.layers.graph_decoders')
 class GraphDecoder(tf.keras.layers.Layer):
     def __init__(
         self,
@@ -38,11 +40,11 @@ class GraphDecoder(tf.keras.layers.Layer):
         super().__init__(*args, **kwargs)
         self._latent_size = latent_size
         self._num_hidden_layers = num_hidden_layers
-        self._activation = activation
+        self._activation = tf.keras.activations.get(activation)
         self._use_bias = use_bias
-        self._kernel_regularizer = kernel_regularizer
-        self._bias_regularizer = bias_regularizer
-        self._activity_regularizer = activity_regularizer
+        self._kernel_regularizer = tf.keras.regularizers.get(kernel_regularizer)
+        self._bias_regularizer = tf.keras.regularizers.get(bias_regularizer)
+        self._activity_regularizer = tf.keras.regularizers.get(activity_regularizer)
         self._should_layer_normalize = should_layer_normalize
         self._node_output_spec_map = output_specs.node_sets
         self._edge_output_spec_map = output_specs.edge_sets
@@ -54,6 +56,32 @@ class GraphDecoder(tf.keras.layers.Layer):
         decoded_edge_sets = self._decode_edge_sets(graph.edge_sets)
         decoded_graph = graph._replace(node_sets=decoded_node_sets, edge_sets=decoded_edge_sets)
         return decoded_graph
+
+    def get_config(self) -> Dict[str, Any]:
+        config = super().get_config()
+        config.update(
+            output_node_set_specs=self._node_output_spec_map,
+            output_edge_set_specs=self._edge_output_spec_map,
+            latent_size=self._latent_size,
+            num_hidden_layers=self._num_hidden_layers,
+            activation=tf.keras.activations.serialize(self._activation),
+            use_bias=self._use_bias,
+            kernel_regularizer=tf.keras.regularizers.serialize(self._kernel_regularizer),
+            bias_regularizer=tf.keras.regularizers.serialize(self._bias_regularizer),
+            activity_regularizer=tf.keras.regularizers.serialize(self._activity_regularizer),
+            should_layer_normalize=self._should_layer_normalize,
+        )
+        return config
+
+    @classmethod
+    def from_config(cls, config: Dict[str, Any]):
+        output_node_set_specs = config.pop('output_node_set_specs')
+        output_edge_set_specs = config.pop('output_edge_set_specs')
+        output_specs = OutputGraphSpec(output_node_set_specs, output_edge_set_specs)
+        config.update(
+            output_specs=output_specs,
+        )
+        return super().from_config(config)
 
     @abc.abstractmethod
     def _create_decoder(self, output_size: int) -> tf.keras.layers.Layer:
