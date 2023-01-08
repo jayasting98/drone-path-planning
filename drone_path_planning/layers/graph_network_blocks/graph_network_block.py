48003ff0fbe8d2a7926712ea23b08f319d510126
Jay Aljelo Ting <65202977+jayasting98@users.noreply.github.com>
2023-01-08 18:01:43 +0800

    Make graph network block serializable

--- drone_path_planning/layers/graph_network_blocks/graph_network_block.py
+++ drone_path_planning/layers/graph_network_blocks/graph_network_block.py
@@ -1,4 +1,5 @@
 import abc
+from typing import Any
 from typing import Dict
 
 import tensorflow as tf
@@ -9,6 +10,7 @@ from drone_path_planning.layers.edge_to_node_aggregators import EdgeToNodeAggreg
 from drone_path_planning.layers.node_processors import NodeProcessor
 
 
+@tf.keras.utils.register_keras_serializable('drone_path_planning.layers.graph_network_blocks')
 class GraphNetworkBlock(tf.keras.layers.Layer):
     def __init__(
         self,
@@ -26,11 +28,11 @@ class GraphNetworkBlock(tf.keras.layers.Layer):
         super().__init__(*args, **kwargs)
         self._latent_size = latent_size
         self._num_hidden_layers = num_hidden_layers
-        self._activation = activation
+        self._activation = tf.keras.activations.get(activation)
         self._use_bias = use_bias
-        self._kernel_regularizer = kernel_regularizer
-        self._bias_regularizer = bias_regularizer
-        self._activity_regularizer = activity_regularizer
+        self._kernel_regularizer = tf.keras.regularizers.get(kernel_regularizer)
+        self._bias_regularizer = tf.keras.regularizers.get(bias_regularizer)
+        self._activity_regularizer = tf.keras.regularizers.get(activity_regularizer)
         self._should_layer_normalize = should_layer_normalize
 
     def build(self, input_shape):
@@ -43,6 +45,20 @@ class GraphNetworkBlock(tf.keras.layers.Layer):
         new_edge_node_graph = self._process_node_sets(new_edge_graph)
         new_edge_node_residual_graph = new_edge_node_graph + graph
         return new_edge_node_residual_graph
+
+    def get_config(self) -> Dict[str, Any]:
+        config = super().get_config()
+        config.update(
+            latent_size=self._latent_size,
+            num_hidden_layers=self._num_hidden_layers,
+            activation=tf.keras.activations.serialize(self._activation),
+            use_bias=self._use_bias,
+            kernel_regularizer=tf.keras.regularizers.serialize(self._kernel_regularizer),
+            bias_regularizer=tf.keras.regularizers.serialize(self._bias_regularizer),
+            activity_regularizer=tf.keras.regularizers.serialize(self._activity_regularizer),
+            should_layer_normalize=self._should_layer_normalize,
+        )
+        return config
 
     @abc.abstractmethod
     def _create_edge_processor(self) -> EdgeProcessor:
