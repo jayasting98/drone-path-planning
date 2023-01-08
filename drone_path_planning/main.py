f0d42ae9f5bf1b32a7fd15a74a2205388d417d7d
Jay Aljelo Ting <65202977+jayasting98@users.noreply.github.com>
2023-01-08 15:16:01 +0800

    Refactor main and train routine to use scenarios and trainers

--- drone_path_planning/main.py
+++ drone_path_planning/main.py
@@ -1,47 +1,17 @@
 import argparse
+from typing import Dict
+from typing import Type
 
-from drone_path_planning.agents import SingleChaserSingleTargetAgent
-from drone_path_planning.environments import SingleChaserSingleMovingTargetEnvironment
-from drone_path_planning.graphs import OutputGraphSpec
 from drone_path_planning.routines import ROUTINES
-from drone_path_planning.utilities.constants import ANTI_CLOCKWISE
-from drone_path_planning.utilities.constants import BACKWARD
-from drone_path_planning.utilities.constants import CLOCKWISE
-from drone_path_planning.utilities.constants import FORWARD
-from drone_path_planning.utilities.constants import REST
+from drone_path_planning.scenarios import Scenario
+from drone_path_planning.scenarios import SingleChaserSingleMovingTargetScenario
 
 
 _PROGRAM_DESCRIPTION = 'Use reinforcement learning to train a drone to plan its path.'
 
 
-_SCENARIO_PARAMETERS = {
-    'single-chaser_single-moving-target': {
-        'agent': SingleChaserSingleTargetAgent,
-        'agent_parameters': dict(
-            output_specs=OutputGraphSpec(
-                node_sets={
-                    'self': [
-                        {
-                            REST: 1,
-                            FORWARD: 1,
-                            BACKWARD: 1,
-                            ANTI_CLOCKWISE: 1,
-                            CLOCKWISE: 1,
-                        }
-                    ],
-                },
-                edge_sets=dict(),
-            ),
-            latent_size=128,
-            num_hidden_layers=2,
-            num_message_passing_steps=1,
-            tau=0.08,
-        ),
-        'training_environment': SingleChaserSingleMovingTargetEnvironment,
-        'training_environment_parameters': dict(),
-        'validation_environment': SingleChaserSingleMovingTargetEnvironment,
-        'validation_environment_parameters': dict(),
-    },
+_SCENARIOS: Dict[str, Type[Scenario]] = {
+    'single-chaser_single-moving-target': SingleChaserSingleMovingTargetScenario,
 }
 
 
@@ -50,7 +20,7 @@ def _parse_args() -> argparse.Namespace:
         description=_PROGRAM_DESCRIPTION,
     )
     parser.add_argument('routine', choices=ROUTINES)
-    parser.add_argument('scenario', choices=_SCENARIO_PARAMETERS)
+    parser.add_argument('scenario', choices=_SCENARIOS)
     args = parser.parse_args()
     return args
 
@@ -58,7 +28,7 @@ def _parse_args() -> argparse.Namespace:
 def main():
     args: argparse.Namespace = _parse_args()
     routine = ROUTINES[args.routine]
-    scenario = _SCENARIO_PARAMETERS[args.scenario]
+    scenario = _SCENARIOS[args.scenario]()
     routine(scenario)
 
 
