d1a92d3cb265f895d870ab6a2b1ab22401b40187
Jay Aljelo Ting <65202977+jayasting98@users.noreply.github.com>
2023-01-08 18:07:48 +0800

    Make multi layer perceptron graph processor serializable

--- drone_path_planning/layers/graph_processors/multi_layer_perceptron_graph_processor.py
+++ drone_path_planning/layers/graph_processors/multi_layer_perceptron_graph_processor.py
@@ -1,7 +1,13 @@
+from typing import Any
+from typing import Dict
+
+import tensorflow as tf
+
 from drone_path_planning.layers.graph_network_blocks import MultiLayerPerceptronGraphNetworkBlock
 from drone_path_planning.layers.graph_processors.graph_processor import GraphProcessor
 
 
+@tf.keras.utils.register_keras_serializable('drone_path_planning.layers.graph_processors')
 class MultiLayerPerceptronGraphProcessor(GraphProcessor):
     def __init__(
         self,
@@ -20,12 +26,26 @@ class MultiLayerPerceptronGraphProcessor(GraphProcessor):
         super().__init__(num_message_passing_steps, *args, **kwargs)
         self._latent_size = latent_size
         self._num_hidden_layers = num_hidden_layers
-        self._activation = activation
+        self._activation = tf.keras.activations.get(activation)
         self._use_bias = use_bias
-        self._kernel_regularizer = kernel_regularizer
-        self._bias_regularizer = bias_regularizer
-        self._activity_regularizer = activity_regularizer
+        self._kernel_regularizer = tf.keras.regularizers.get(kernel_regularizer)
+        self._bias_regularizer = tf.keras.regularizers.get(bias_regularizer)
+        self._activity_regularizer = tf.keras.regularizers.get(activity_regularizer)
         self._should_layer_normalize = should_layer_normalize
+
+    def get_config(self) -> Dict[str, Any]:
+        config = super().get_config()
+        config.update(
+            latent_size=self._latent_size,
+            num_hidden_layers=self._num_hidden_layers,
+            activation=tf.keras.activations.serialize(self._activation),
+            use_bias=self._use_bias,
+            kernel_regularizer=tf.keras.regularizers.serialize(self._kernel_regularizer),
+            bias_regularizer=tf.keras.regularizers.serialize(self._bias_regularizer),
+            activity_regularizer=tf.keras.regularizers.serialize(self._activity_regularizer),
+            should_layer_normalize=self._should_layer_normalize,
+        )
+        return config
 
     def _create_graph_network_block(self):
         return MultiLayerPerceptronGraphNetworkBlock(
