31d7ff718f863ca79832ff6ae389cafad8c7d8a6
Jay Aljelo Ting <65202977+jayasting98@users.noreply.github.com>
2023-01-08 16:32:27 +0800

    Make edge-to-node aggregator serializable

--- drone_path_planning/layers/edge_to_node_aggregators/edge_to_node_aggregator.py
+++ drone_path_planning/layers/edge_to_node_aggregators/edge_to_node_aggregator.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 
 
+@tf.keras.utils.register_keras_serializable('drone_path_planning.layers.edge_to_node_aggregators')
 class EdgeToNodeAggregator(tf.keras.layers.Layer):
     pass
