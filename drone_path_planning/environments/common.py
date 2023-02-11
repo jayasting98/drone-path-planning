a03c522b4603378ae5560f2a7bafb909926edd28
Jay Aljelo Ting <65202977+jayasting98@users.noreply.github.com>
2023-02-11 16:09:45 +0800

    Clean environments common code

--- drone_path_planning/environments/common.py
+++ drone_path_planning/environments/common.py
@@ -1,10 +1,13 @@
 import tensorflow as tf
 
 
-def find_direction(angular_displacement: tf.Tensor):
-  direction = tf.concat([
-      tf.math.cos(angular_displacement),
-      tf.math.sin(angular_displacement),
-      tf.zeros(tf.shape(angular_displacement)),
-  ], axis=-1)
-  return direction
+def find_direction(angular_displacement: tf.Tensor) -> tf.Tensor:
+    direction = tf.concat(
+        [
+            tf.math.cos(angular_displacement),
+            tf.math.sin(angular_displacement),
+            tf.zeros(tf.shape(angular_displacement)),
+        ],
+        axis=-1,
+    )
+    return direction
