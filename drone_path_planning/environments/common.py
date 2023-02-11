97b8b4f5b046f60f232fb83a2ef1495af7b7517d
Jay Aljelo Ting <65202977+jayasting98@users.noreply.github.com>
2023-02-11 17:16:41 +0800

    Add common utility cartesian product functions

--- drone_path_planning/environments/common.py
+++ drone_path_planning/environments/common.py
@@ -11,3 +11,30 @@ def find_direction(angular_displacement: tf.Tensor) -> tf.Tensor:
         axis=-1,
     )
     return direction
+
+
+def find_cartesian_product(p: tf.Tensor, q: tf.Tensor) -> tf.Tensor:
+    multiples = [1 for _ in range(len(tf.shape(p)) + 1)]
+    horizontal_multiples = multiples[:]
+    horizontal_multiples[1] = tf.shape(q)[0]
+    horizontally_tiled_p = tf.tile(tf.expand_dims(p, 1), horizontal_multiples)
+    vertical_multiples = multiples[:]
+    vertical_multiples[0] = tf.shape(p)[0]
+    vertically_tiled_q = tf.tile(tf.expand_dims(q, 0), vertical_multiples)
+    cartesian_product = tf.stack([horizontally_tiled_p, vertically_tiled_q], axis=2)
+    return cartesian_product
+
+
+def find_pairs_from_cartesian_product(cartesian_product: tf.Tensor) -> tf.Tensor:
+    pairs_shape = [-1]
+    pairs_shape.extend(tf.shape(cartesian_product)[2:])
+    pairs = tf.reshape(cartesian_product, pairs_shape)
+    return pairs
+
+
+def find_cartesian_square_pairs_with_distinct_elements(cartesian_square_pairs: tf.Tensor) -> tf.Tensor:
+    size2 = tf.shape(cartesian_square_pairs)[0]
+    size = tf.cast(tf.math.sqrt(tf.cast(size2, tf.dtypes.double)) + 0.5, tf.dtypes.int32)
+    mask = tf.range(size * size) % (size + 1) != 0
+    pairs_with_distinct_elements = tf.boolean_mask(cartesian_square_pairs, mask)
+    return pairs_with_distinct_elements
