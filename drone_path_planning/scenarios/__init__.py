ba46f63807833333206d44cf7b63b0cd36841214
Jay Aljelo Ting <65202977+jayasting98@users.noreply.github.com>
2023-02-22 11:34:15 +0800

    Add two-chasers single-moving-target scenario

--- drone_path_planning/scenarios/__init__.py
+++ drone_path_planning/scenarios/__init__.py
@@ -4,9 +4,11 @@ from typing import Type
 from drone_path_planning.scenarios.scenario import Scenario
 from drone_path_planning.scenarios.one_chaser_single_moving_target_scenario import OneChaserSingleMovingTargetScenario
 from drone_path_planning.scenarios.single_chaser_single_moving_target_scenario import SingleChaserSingleMovingTargetScenario
+from drone_path_planning.scenarios.two_chasers_single_moving_target_scenario import TwoChasersSingleMovingTargetScenario
 
 
 SCENARIOS: Dict[str, Type[Scenario]] = {
     'one-chaser_single-moving-target': OneChaserSingleMovingTargetScenario,
     'single-chaser_single-moving-target': SingleChaserSingleMovingTargetScenario,
+    'two-chasers_single-moving-target': TwoChasersSingleMovingTargetScenario,
 }
