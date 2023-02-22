3923e6b24d129ddc40229abdc31d58ebdfd1e49b
Jay Aljelo Ting <65202977+jayasting98@users.noreply.github.com>
2023-02-22 11:20:21 +0800

    Add multi-agent-related constants

--- drone_path_planning/utilities/constants.py
+++ drone_path_planning/utilities/constants.py
@@ -2,14 +2,30 @@ SELF_ANGULAR_VELOCITY: str = 'self_angular_velocity'
 TARGET_ANGULAR_VELOCITY: str = 'target_angular_velocity'
 TARGET_RELATIVE_DISPLACEMENT: str = 'target_relative_displacement'
 TARGET_RELATIVE_VELOCITY: str = 'target_relative_velocity'
+AGENT_ANGULAR_VELOCITIES: str = 'agent_angular_velocities'
+CHASER_RELATIVE_DISPLACEMENTS: str = 'chaser_relative_displacements'
+CHASER_RELATIVE_VELOCITIES: str = 'chaser_relative_velocities'
+CHASER_ANGULAR_VELOCITIES: str = 'chaser_angular_velocities'
+TARGET_RELATIVE_DISPLACEMENTS: str = 'target_relative_displacements'
+TARGET_RELATIVE_VELOCITIES: str = 'target_relative_velocities'
+TARGET_ANGULAR_VELOCITIES: str = 'target_angular_velocities'
 
 
 SELF: str = 'self'
+AGENT: str = 'agent'
+CHASER: str = 'chaser'
 TARGET: str = 'target'
 
 
 SELF_TARGET: str = 'self_target'
 TARGET_SELF: str = 'target_self'
+AGENT_CHASER: str = 'agent_chaser'
+CHASER_AGENT: str = 'chaser_agent'
+AGENT_TARGET: str = 'agent_target'
+TARGET_AGENT: str = 'target_agent'
+CHASER_CHASER: str = 'chaser_chaser'
+CHASER_TARGET: str = 'chaser_target'
+TARGET_CHASER: str = 'target_chaser'
 
 
 ANTI_CLOCKWISE: str = 'anti_clockwise'
